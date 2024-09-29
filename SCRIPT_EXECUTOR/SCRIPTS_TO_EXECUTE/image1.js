89255a74b7b9d212ebbed35154e8a5a38d2c8471
lapmic5001 <michaelvlaposta@gmail.com>
2024-09-29 18:25:03 -0400

    ENACTOREXACTOREXECUTION

---
+++ SCRIPT_EXECUTOR/SCRIPTS_TO_EXECUTE/image1.js
@@ -0,0 +1,8 @@
+// JavaScript file containing hex color data for image1
+const image1HexArray = [
+    "#FFFFFF", "#000000", "#FF5733", // and so on for all pixels
+    // This array should contain all the hex values of the image's pixels
+];
+
+// The secret message associated with image1
+const message1 = "DA SECRET MESSAGE";
